c0ab63d545e258733b9166a186d3a1b82f37c57f
AJ <119537954+fidind3211@users.noreply.github.com>
2023-12-16 23:00:06 -0500

    e

--- public/script.js
+++
@@ -1,9 +0,0 @@
-// Background Animation Interaction
-const background = document.querySelector('.background');
-
-document.addEventListener('mousemove', (e) => {
-    const mouseX = e.clientX / window.innerWidth;
-    const mouseY = e.clientY / window.innerHeight;
-
-    background.style.transform = `translate(-${mouseX * 50}px, -${mouseY * 50}px)`;
-});
